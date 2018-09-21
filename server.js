d1350754c342db20fde24dba9808de3c0c8cc187
Han Sung Bae <hansungbae@Hans-MacBook-Air.local>
2018-09-20 21:48:47 -0400

    heroku

--- server.js
+++ server.js
@@ -28,17 +28,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/thedailyScraper", { useNewUrlParser: true });
-
-
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// // Set mongoose to leverage built in JavaScript ES6 Promises
 // // Connect to the Mongo DB
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/thedailyScraper", { useNewUrlParser: true });
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 // Routes
